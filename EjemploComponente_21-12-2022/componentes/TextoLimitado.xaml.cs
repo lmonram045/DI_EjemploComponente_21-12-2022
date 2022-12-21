4418677b1fc08b311c68e684322b0ae519161cf1
lmr87 <lmr87@DESKTOP-OGJRNU7>
2022-12-21 18:51:10 +0100

    Agregar archivos de proyecto.

---
+++ EjemploComponente_21-12-2022/componentes/TextoLimitado.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EjemploComponente_21_12_2022.componentes
+{
+    /// <summary>
+    /// Lógica de interacción para TextoLimitado.xaml
+    /// </summary>
+    public partial class TextoLimitado : UserControl
+    {
+        public TextoLimitado()
+        {
+            InitializeComponent();
+            this.DataContext = this;
+        }
+        public string Titulo { get; set; }
+        public int Longitud { get; set; }
+}
+}
